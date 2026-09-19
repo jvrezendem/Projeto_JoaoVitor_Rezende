11e585c64ac0b66453c5d1aaebdd5c1b5c0aafe0
jvrezendem <jrezendemarciano@gmail.com>
2026-09-19 20:05:20 -0300

    feat:Criação da classe D

---
+++ D.java
@@ -0,0 +1,13 @@
+public class D {
+    private String d1;
+    private int d2;
+
+    public void md1(){
+        System.out.println("md1");
+    }
+
+    public void md2(){
+        System.out.println("md2");
+    }
+
+}
