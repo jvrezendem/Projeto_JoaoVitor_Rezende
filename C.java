e8d2c9eacf354729bdbff17ea1349439dc8dd5fa
jvrezendem <jrezendemarciano@gmail.com>
2026-09-19 19:35:49 -0300

    feat:Criação da classe C

---
+++ C.java
@@ -0,0 +1,12 @@
+public class C {
+    private String c1;
+    private int c2;
+
+    public void mc1(){
+        System.out.println("mc1");
+    }
+
+    public void mc2(){
+        System.out.println("mc2");
+    }
+}
