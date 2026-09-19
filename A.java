40ab86c1b136336782ca40ed3c12284f0b8adb37
jvrezendem <jrezendemarciano@gmail.com>
2026-09-19 19:00:20 -0300

    feat: criação da classe A

---
+++ A.java
@@ -0,0 +1,14 @@
+public class A {
+
+    private int a1;
+    private float a2;
+
+    public void ma1(){
+        System.out.println("ma1");
+    }
+
+    public void ma2(){
+        System.out.println("ma2");
+    }
+
+}
